fc6e0b575fea162ba23b6d999e3b025c5024fa26
Jeffrey Bergier <jeffburg@jeffburg.com>
2018-08-01 15:24:37 -0700

    added MIT license info

--- XPUIKit/XPUIKit_shared/XPUIProtocols.h
+++ XPUIKit/XPUIKit_shared/XPUIProtocols.h
@@ -1,5 +1,5 @@
 //
-//  Header.h
+//  XPUIProtocols.h
 //  XPUIKit
 //
 //  Created by Jeffrey Bergier on 01/08/2018.
@@ -30,15 +30,4 @@
 
 @import Foundation;
 
-//! Project version number for XPUIKit_macOS.
-FOUNDATION_EXPORT double XPUIKit_macOSVersionNumber;
 
-//! Project version string for XPUIKit_macOS.
-FOUNDATION_EXPORT const unsigned char XPUIKit_macOSVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <XPUIKit_macOS/PublicHeader.h>
-#if TARGET_OS_IPHONE
-#import <XPUIKit/XPUIKit_iOS.h>
-#else
-#import <XPUIKit/XPUIKit_macOS.h>
-#endif
